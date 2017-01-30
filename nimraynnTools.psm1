02efa5753b0ce73dd488795d5a1509ff87a33a01
Mike Oldfield <michael.oldfield@capita.co.uk>
2017-01-30 16:17:31 +0000

    New manifest & module files

--- nimraynnTools.psm1
+++ nimraynnTools.psm1
@@ -1,5 +1,6 @@
 ### nimraynnTools PowerShell Module Pack
 
+### Main Module File
 ### Module Version 1.1
 ### Last Updated: 30.01.2017
 
@@ -19,11 +20,18 @@ Get-ChildItem -Path $functionsDir -Filter '*.ps1' |
                 . $_.FullName | Out-Null
         }
 
-# Test the module manifest 
+# Test the module manifest is correct
 $module = Test-ModuleManifest -Path (Join-Path -Path $PSScriptRoot -ChildPath 'nimraynnTools.psd1' -Resolve)
+
+# Check if a valid manifest was returned
 if( -not $module )
 {
+    
+    # If not, kill it now
     return
+    
 }
 
+# If all is well, then export all the functions listed in $module
+# This should be anything listed in the FunctionsToExport section of the manifest
 Export-ModuleMember -Alias '*' -Function ([string[]]$module.ExportedFunctions.Keys)
